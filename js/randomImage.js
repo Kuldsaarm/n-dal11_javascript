3d86aac4d93b8ec538fc24dcf959333db90a3f38
Marjana Kul <marjana.kuldsaar@tktk.ee>
2023-12-22 16:17:44 +0200

    fixes

--- js/randomImage.js
+++ js/randomImage.js
@@ -1,4 +1,4 @@
-const images = ["/img/pilt1.jpg", "/img/pilt2.jpg", "/img/pilt3.jpg", "/img/pilt4.jpg"];
+const images = ["img/pilt1.jpg", "img/pilt2.jpg", "img/pilt3.jpg", "img/pilt4.jpg"];
 const currentMainImage = document.querySelector(".image-container img");
 const myButton = document.querySelector('button');
 
